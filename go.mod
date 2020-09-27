0df1af741b97a07c51034b1b846e0e2468e983c8
s_zer0 <shoha2105@gmail.com>
2020-09-27 21:32:05 +0500

    v2 внесены изменения в функции Avg и TotalInCategory, не учитываются платежи statusFail

--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
-module github.com/s-zer0/stats
+module github.com/s-zer0/stats/v2
 
 go 1.15
 
-require github.com/s-zer0/bank v0.0.0-20200927133614-d55aa466594c
+require (
+	github.com/s-zer0/bank v0.0.0-20200927133614-d55aa466594c
+	github.com/s-zer0/bank/v2 v2.0.0
+	github.com/s-zer0/stats v1.0.0 // indirect
+)
--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -1,14 +1,18 @@
 package stats
 
-import "github.com/s-zer0/bank/pkg/types"
+import "github.com/s-zer0/bank/v2/pkg/types"
+
+const statusFails = types.StatusFail
 
 // Avg рассчитывает среднюю сумму платежа.
 func Avg(payments []types.Payment) types.Money {
 	avg := types.Money(0)
 	quantity := types.Money(0)
 	for _, v := range payments {
+		if v.Status != statusFails {
 		avg += v.Amount
 		quantity ++
+		}
 	}
 	return avg/quantity
 }
@@ -16,12 +20,13 @@ func Avg(payments []types.Payment) types.Money {
 // TotalInCategory находит сумму покупок в определённой категории.
 func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
 	totalCategory := types.Money(0)
-
+	
 	for _, payment := range payments {
-		if payment.Category == category {
-			totalCategory += payment.Amount
+		if payment.Status != statusFails {
+			if payment.Category == category {
+				totalCategory += payment.Amount
 		}
-		
+	}
 	}
 	return totalCategory
 }
