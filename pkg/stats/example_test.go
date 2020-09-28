0d1e5c6c27fe8fe1a16908a5e878960dc5749ecd
s_zer0 <shoha2105@gmail.com>
2020-09-28 21:48:51 +0500

    Добавлена функция CategoriesAvg для расчёта средней суммы платежей по категориям и тесты к нему

--- pkg/stats/example_test.go
+++ pkg/stats/example_test.go
@@ -2,61 +2,84 @@ package stats
 
 import (
 	"github.com/s-zer0/bank/v2/pkg/types"
-	"fmt")
+  //"fmt"
+  "testing"
+  "reflect")
 
-func ExampleAvg(){
-	payments := []types.Payment{
-	{
-	    ID:2,
-      Amount:53_00,
-      Category: "Cat",
-      Status: "FAIL",
+// func ExampleAvg(){
+// 	payments := []types.Payment{
+// 	{
+// 	    ID:2,
+//       Amount:53_00,
+//       Category: "Cat",
+//       Status: "FAIL",
 
-	},
+// 	},
 	
-    {
-      ID:1,
-      Amount:51_00,
-      Category: "Cat",
-      Status: "OK",
-	},
+//     {
+//       ID:1,
+//       Amount:51_00,
+//       Category: "Cat",
+//       Status: "OK",
+// 	},
 	
-    {
-      ID:3,
-      Amount:52_00,
-      Category: "Cat",
-      Status: "INPROGRESS",
-    },
-  }
+//     {
+//       ID:3,
+//       Amount:52_00,
+//       Category: "Cat",
+//       Status: "INPROGRESS",
+//     },
+//   }
 
-  fmt.Println(Avg(payments))
+//   fmt.Println(Avg(payments))
 
-  //Output: 5150
-}
+//   //Output: 5150
+// }
 
-func ExampleTotalInCategory(){
+// func ExampleTotalInCategory(){
+//   payments := []types.Payment{
+//     {
+//       ID:2,
+//       Amount:53_00,
+//       Category: "Cafe",
+//       Status: "INPROGRESS",
+//     },
+//     {
+//       ID:1,
+//       Amount:51_00,
+//       Category: "Cafe",
+//       Status: "OK",
+//     },
+//     {
+//       ID:3,
+//       Amount:52_00,
+//       Category: "Restaurant",
+//       Status: "FAIL",
+//     },
+//   }
+
+//   fmt.Println(TotalInCategory(payments, "Cafe"))
+
+//   //Output: 10400
+// }
+
+func TestCategoriesAvg(t *testing.T) {
   payments := []types.Payment{
-    {
-      ID:2,
-      Amount:53_00,
-      Category: "Cafe",
-      Status: "INPROGRESS",
-    },
-    {
-      ID:1,
-      Amount:51_00,
-      Category: "Cafe",
-      Status: "OK",
-    },
-    {
-      ID:3,
-      Amount:52_00,
-      Category: "Restaurant",
-      Status: "FAIL",
-    },
+    {ID:1, Category: "auto", Amount: 1_000_00},
+    {ID:2, Category: "food", Amount: 2_000_00},
+    {ID:3, Category: "auto", Amount: 4_000_00},
+    {ID:4, Category: "auto", Amount: 4_000_00},
+    {ID:5, Category: "fun", Amount: 5_000_00},
+  } 
+  expected := map[types.Category]types.Money{
+    "auto": 3_000_00,
+    "food": 2_000_00,
+    "fun": 5_000_00,
   }
 
-  fmt.Println(TotalInCategory(payments, "Cafe"))
+  result := CategoriesAvg(payments)
 
-  //Output: 10400
+  if !reflect.DeepEqual (expected, result){
+    t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
+  }
 }
--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -2,31 +2,44 @@ package stats
 
 import "github.com/s-zer0/bank/v2/pkg/types"
 
-const statusFails = types.StatusFail
+// const statusFails = types.StatusFail
 
-// Avg рассчитывает среднюю сумму платежа.
-func Avg(payments []types.Payment) types.Money {
-	avg := types.Money(0)
-	quantity := types.Money(0)
-	for _, v := range payments {
-		if v.Status != statusFails {
-		avg += v.Amount
-		quantity ++
-		}
-	}
-	return avg/quantity
-}
+// // Avg рассчитывает среднюю сумму платежа.
+// func Avg(payments []types.Payment) types.Money {
+// 	avg := types.Money(0)
+// 	quantity := types.Money(0)
+// 	for _, v := range payments {
+// 		if v.Status != statusFails {
+// 		avg += v.Amount
+// 		quantity ++
+// 		}
+// 	}
+// 	return avg/quantity
+// }
 
-// TotalInCategory находит сумму покупок в определённой категории.
-func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
-	totalCategory := types.Money(0)
+// // TotalInCategory находит сумму покупок в определённой категории.
+// func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
+// 	totalCategory := types.Money(0)
 	
+// 	for _, payment := range payments {
+// 		if payment.Status != statusFails {
+// 			if payment.Category == category {
+// 				totalCategory += payment.Amount
+// 		}
+// 	}
+// 	}
+// 	return totalCategory
+// }
+
+func CategoriesAvg(payments []types.Payment) map[types.Category]types.Money {
+	categories := map[types.Category]types.Money{}
+	quantity := map[types.Category]types.Money{}	
 	for _, payment := range payments {
-		if payment.Status != statusFails {
-			if payment.Category == category {
-				totalCategory += payment.Amount
-		}
+		categories[payment.Category] += payment.Amount
+		quantity[payment.Category] += 1
 	}
+	for key := range categories {
+		categories[key] = (categories[key]/quantity[key])
 	}
-	return totalCategory
+	return categories
 }
